18f72ae443af47d9ac2514cfef34e7419eb473f6
Giovanni <gio.fon@gmail.com>
2017-06-14 10:22:22 +0200

    favicon

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/favicon.ico', function(req, res) {
+    res.send(204);
+});
 router.get('/', function(req, res, next) {
   res.render('index');
 });
